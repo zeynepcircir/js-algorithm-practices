de9f75344fd58dc09a5069922dbe282c54da1105
zeynep.circir <zynpcrcr@gmail.com>
2025-04-02 11:40:13 +0300

    Fibonacci algorithm solve

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+const fibonacci = (n) => {
+    let arr = [];
+    if(n===0){
+        arr.push(0);
+    }else if(n===1){
+        arr.push(1);
+    }else{
+        arr.push(0,1);
+        for (let i = 2; i < n; i++) {
+            let u = arr[i-2] + arr[i-1];
+            arr.push(u);
+        }
+        
+    }
+    console.log(arr);
+}
+
+fibonacci(2);
